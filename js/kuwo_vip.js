9e382bdef1da9a5c271117fa059029e81f50681e
Llealf <67813929+kiki0-zz@users.noreply.github.com>
2025-12-17 17:07:51 +0800

    更新 kuwo_vip.js

--- js/kuwo_vip.js
+++ js/kuwo_vip.js
@@ -1,16 +1,21 @@
-// Kuwo VIP Fake Script
 let body = $response.body;
 
 try {
   let obj = JSON.parse(body);
 
-  if (obj.data) {
-    obj.data.isVip = 1;
-    obj.data.vip = 1;
-    obj.data.vipLevel = 8;
-    obj.data.expire = "2099-12-31";
-    obj.data.vipType = "svip";
-  }
+  // 多层兜底
+  if (!obj.data) obj.data = {};
+
+  obj.data.isVip = 1;
+  obj.data.vip = 1;
+  obj.data.vipLevel = 9;
+  obj.data.vipType = "svip";
+  obj.data.expire = "2099-12-31";
+  obj.data.vipExpire = 4102444800;
+
+  // 一些版本读这里
+  obj.isVip = 1;
+  obj.vip = 1;
 
   body = JSON.stringify(obj);
 } catch (e) {}
